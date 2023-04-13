08d30d3772278c816c4b091f8f1e057a4ab286b2
Afonso Bernardes <afonso.bernardes@tecnico.ulisboa.pt>
2023-04-13 18:12:33 +0100

    Both encrypt and decrypt are now just one function, ceaser_cipher().

--- CeaserCipher/main.py
+++ CeaserCipher/main.py
@@ -4,46 +4,31 @@ alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'
 
 # Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs. Shift the plain message and print
 # encoded message.
-def encrypt(plain_message, shift_value):
+def ceaser_cipher(message, shift_value, direction):
     shifted_alphabet = alphabet[shift_value:] + alphabet[:shift_value]
-    encrypted_message = ""
-    for letter in plain_message:
-        # If it's a number or symbol, simply add that.
+    processed_message = ""
+    for letter in message:
         if letter in alphabet:
-            letter_idx = alphabet.index(letter)
-            encrypted_message += shifted_alphabet[letter_idx]
+            if direction == "ENCODE":
+                letter_idx = alphabet.index(letter)
+                processed_message += shifted_alphabet[letter_idx]
+            elif direction == "DECODE":
+                letter_idx = shifted_alphabet.index(letter)
+                processed_message += alphabet[letter_idx]
         else:
-            encrypted_message += letter
+            # If it's a number or symbol, simply add that.
+            processed_message += letter
 
-    print(f"The encoded message is: {encrypted_message}")
-
-
-def decrypt(encrypted_message, shift_value):
-    shifted_alphabet = alphabet[shift_value:] + alphabet[:shift_value]
-    decrypted_message = ""
-    for letter in encrypted_message:
-        # If it's a number or symbol, simply add that.
-        if letter in alphabet:
-            letter_idx = shifted_alphabet.index(letter)
-            decrypted_message += alphabet[letter_idx]
-        else:
-            decrypted_message += letter
-
-    print(f"The decoded message is: {decrypted_message}")
+    print(f"The decoded message is: {processed_message}")
 
 
 # This loop asks for the user to encode/decode, if input is not valid, asks again.
 while True:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").upper()
-    if direction == "ENCODE":
+    if direction == "ENCODE" or direction == "DECODE":
         text = input("Type your message: ").upper()
         shift = int(input("Type the shift number: "))
-        encrypt(plain_message=text, shift_value=shift)
-        break
-    elif direction == "DECODE":
-        text = input("Type your message: ").upper()
-        shift = int(input("Type the shift number: "))
-        decrypt(encrypted_message=text, shift_value=shift)
+        ceaser_cipher(message=text, shift_value=shift, direction=direction)
         break
     else:
         print("Invalid input, try again.\n")
