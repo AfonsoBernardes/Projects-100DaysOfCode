08f3a9b8edd1af87f3d6847936d9d982f0c5c562
Afonso Bernardes <afonso.bernardes@tecnico.ulisboa.pt>
2023-04-17 17:21:12 +0100

    Player loop (sketch) finished.

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -1,3 +1,4 @@
+import card
 from blackjack_ascii import logo
 import deck
 import player
@@ -10,6 +11,7 @@ import player
 # Use the following list as the deck of cards: [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]
 # The cards in the list have equal probability of being drawn.
 # The computer is the dealer.
+from card import Card
 
 print(logo)
 
@@ -23,13 +25,63 @@ player_name = input("Hello, welcome to Blackjack. What is your name? ")
 user = player.Player(name=player_name)
 dealer = player.Player(name='Dealer')
 
+# ace = card.Card(rank='A', value=11, suit="Spades", face_up=True)
+
 while play_game:
+    # This is the initial hand. Dealer's second card is hidden.
     user.draw_card(deck)
+    # user.hand.append(ace)
     dealer.draw_card(deck)
     user.draw_card(deck)
     dealer.draw_card(deck)
+    dealer.hand[-1].set_is_face_up(face_up=False)
+
+    user.show_hand()
+    dealer.show_hand()
+
+    # If player has blackjack from the start, do not enter loop.
+    if user.get_hand_score() == 21:
+        print("You have blackjack!")
+    else:
+        # PLAYER'S LOOP
+        not_bust = True
+        while not_bust:
+            while True:
+                user_action = input("\nDo you want to HIT or STAND? ").upper()
+                if user_action == 'HIT' or user_action == 'STAND':
+                    break
+                else:
+                    print('Invalid action, try again.')
+
+            if user_action == 'HIT':
+                user.draw_card(deck)
+                # When player hits and hand's score is above 21, check for aces with value=11 and replace for value=1.
+                if user.get_hand_score() > 21 and user.get_ace_11() is not None:
+                    user.get_ace_11().set_value(1)
+                    user.show_hand()
+                    dealer.show_hand()
+
+                # Player's hand over 21 without an ace to change.
+                elif user.get_hand_score() > 21:
+                    not_bust = False
+                    user.show_hand()
+                    dealer.show_hand()
+                    print("BUST! YOU LOSE!\n")
+                    break
+
+                # Simply display hands after hitting.
+                else:
+                    user.show_hand()
+                    dealer.show_hand()
+            # Break player's loop and move to dealer's.
+            elif user_action == 'STAND':
+                break
+
+    # Flip dealer's card up
+    dealer.hand[-1].set_is_face_up(face_up=True)
+    user.show_hand()
+    dealer.show_hand()
+    if dealer.get_hand_score() < 17:
+        
+
     play_game = False
-
-user.show_hand()
-dealer.show_hand()
-
--- Blackjack/player.py
+++ Blackjack/player.py
@@ -1,13 +1,40 @@
 class Player:
     def __init__(self, name):
+        self.current_score = 0
         self.name = name
         self.hand = []
 
     def draw_card(self, deck):
         self.hand.append(deck.draw_card())
 
+    def has_ace(self):
+        """
+        Check if player has an Ace on his hand.
+        :return: boolean
+        """
+        for card in self.hand:
+            if card.rank == 'A':
+                return True
+        return False
+
+    def get_ace_11(self):
+        """
+        Find (in hand) ace with value equals to 11.
+        :return: card object
+        """
+        for card in self.hand:
+            if card.rank == 'A' and card.get_value() == 11:
+                return card
+
+    def get_hand_score(self):
+        self.current_score = 0
+        for card in self.hand:
+            if card.get_is_face_up():
+                self.current_score += card.get_value()
+        return self.current_score
+
     def show_hand(self):
         print(f"{self.name}'s hand:", end=" ")
         for card in self.hand:
             card.show_card()
-        print("")
+        print(f"(Current hand value: {self.get_hand_score()})")
