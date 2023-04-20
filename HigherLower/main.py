d613162d65824509270f7706b6d006b56b8d3517
Afonso Bernardes <afonso.bernardes@tecnico.ulisboa.pt>
2023-04-20 19:02:17 +0100

    get_options function added.

--- HigherLower/main.py
+++ HigherLower/main.py
@@ -6,8 +6,20 @@ print(high_low_art.logo)
 
 
 def get_options(options_data):
+    """
+    :param options_data: list of dictionaries with several instagram accounts. Contains 'name',
+    'follower_count' (in millions), 'description', and country.
+    :return: option_a, option_b as dictionaries.
+    """
+    print("Who has more followers on Instagram?")
     option_a, option_b = random.choices(options_data, k=2)
+    print(f"Compare A: {option_a['name']}, a {option_a['description'].lower()} from {option_a['country']}.")
+    print(high_low_art.vs)
+    print(f"Against B: {option_b['name']}, a {option_b['description'].lower()} from {option_b['country']}.")
     return option_a, option_b
 
 
-get_options(game_data.data)
+def game():
+    user_score = 0
+    option_a, option_b = get_options(game_data.data)
+    user_answer = input("Who has more followers on Instagram? Type A or B. ")
