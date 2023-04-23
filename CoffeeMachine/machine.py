28d245e10528e24bfdc1331948ce9c44b06068a2
Afonso Bernardes <afonso.bernardes@tecnico.ulisboa.pt>
2023-04-23 18:46:32 +0100

    make_drink(), get_coins() methods implemented. Fully functional.

--- CoffeeMachine/machine.py
+++ CoffeeMachine/machine.py
@@ -1,15 +1,77 @@
 class Machine:
-    def __init__(self, resources: dict):
+    def __init__(self, resources: dict, menu: dict):
         """
         Creates a machine object with initialized with resources from a dictionary.
         """
+        self.menu = menu
+        self.accepted_coins = {"quarters": 0.25, "dimes": 0.10, "nickles": 0.05, "pennies": 0.01}
         self.water = resources['water']
         self.milk = resources['milk']
         self.coffee = resources['coffee']
         self.money = 0
+
+    def get_menu(self):
+        return self.menu
 
     def get_report(self):
         print(f'Water: {self.water}ml')
         print(f'Milk: {self.milk}ml')
         print(f'Coffee: {self.coffee}g')
         print(f'Money: {self.money}€\n')
+
+    def check_resources(self, drink: str):
+        """
+        Checks if machine has enough resources to make drink.
+        :param drink:
+        :return: bool
+        """
+        if self.water < self.menu[drink]['ingredients']['water']:
+            print('Sorry, there is not enough water.')
+            return False
+
+        if self.milk < self.menu[drink]['ingredients']['milk']:
+            print('Sorry, there is not enough milk.')
+            return False
+
+        if self.milk < self.menu[drink]['ingredients']['coffee']:
+            print('Sorry, there is not enough coffee.')
+            return False
+
+        return True
+
+    def get_coins(self):
+        print('\nPlease, insert coins.')
+        num_quarters = int(input('How many quarters? '))
+        num_dimes = int(input('How many dimes? '))
+        num_nickles = int(input('How many nickles? '))
+        num_pennies = int(input('How many pennies? '))
+
+        return num_quarters * self.accepted_coins['quarters'] + num_dimes * self.accepted_coins['dimes'] + \
+            num_nickles * self.accepted_coins['nickles'] + num_pennies * self.accepted_coins['pennies']
+
+    def make_drink(self, drink: str):
+        """
+        Get drink user wants. Ask for the coins. Check if resources are enough and if so make drink:
+            deduct from resources, add money to machine, and give change.
+        :param drink:
+        :return:
+        """
+        total_money = self.get_coins()
+        if total_money < self.menu[drink]['cost']:
+            print(f'\nTotal amount inserted: {total_money}€')
+            print(f"Drink's price: {self.menu[drink]['cost']}")
+            print("Sorry, that's not enough money. Money refunded.\n")
+            return
+
+        # If machine can make drink: Deduct resources, add money, give change.
+        self.water -= self.menu[drink]['ingredients']['water']
+        self.milk -= self.menu[drink]['ingredients']['milk']
+        self.coffee -= self.menu[drink]['ingredients']['coffee']
+        self.money += self.menu[drink]['cost']
+
+        change = total_money - self.menu[drink]['cost']
+        print(f'\nTotal amount inserted: {total_money}€')
+        print(f"Drink's price: {self.menu[drink]['cost']}")
+        print(f"Here is {change:.2f}€ in change.")
+        print(f"Here is your {drink} ☕. Enjoy!\n")
+        return
--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -2,28 +2,17 @@ from menu import MENU, resources
 import sys
 import machine
 
-# TODO-1: Prompt user asking "What would you like?". Check user input.
-coffee_machine = machine.Machine(resources)
+# Prompt user asking "What would you like?". Check user input.
+coffee_machine = machine.Machine(resources, MENU)
 while True:
     user_choice = input('What would you like? Espresso, Latte or Cappuccino. ').lower()
-    if user_choice in MENU:
-        break
+    if user_choice in coffee_machine.get_menu():
+        # Check if resources are sufficient.
+        if coffee_machine.check_resources(drink=user_choice):
+            coffee_machine.make_drink(drink=user_choice)
     elif user_choice == 'report':
         coffee_machine.get_report()
     elif user_choice == 'off':
         sys.exit("Machine is shutting down for maintenance.")
     else:
         print("Invalid input!\n")
-
-# TODO-4: Check if resources are sufficient.
-#         Print:"Sorry there is not enough {resource}.
-#
-# TODO-5: If there are enough resources to make drink, prompt user to insert coins.
-#         Quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-
-# TODO-6: Check if user inserted enough money.
-#         Add drink's money to machine.money.
-#         Offer change.
-
-# TODO-7: Make the coffee.
-#         Deduct resources from machine.
