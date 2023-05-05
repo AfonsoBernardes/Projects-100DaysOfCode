432e34e5ca28884cdc42ccbabc7467f1581fe04b
Afonso Bernardes <afonso.bernardes@tecnico.ulisboa.pt>
2023-05-05 16:06:22 +0100

    Automatically modify template letter to add names.

---
+++ MailMerge/main.py
@@ -0,0 +1,10 @@
+# TODO: Create a letter using starting_letter.txt
+# Iterate names in invited_names.txt
+with open('./Input/Names/invited_names.txt', mode='r') as invited_names:
+    for person_name in invited_names.readlines():
+        # With each name, replace [name] in starting_letter.txt
+        with open('./Input/Letters/starting_letter.txt', mode='r') as starting_letter:
+            customised_letter = starting_letter.read().replace('[name]', f'{person_name.strip()}')
+            # Save customised letter to file and save the letters in the folder "ReadyToSend".
+            with open(f'./Output/ReadyToSend/{person_name.strip()}_Letter.txt', mode='w') as file:
+                file.write(customised_letter)
