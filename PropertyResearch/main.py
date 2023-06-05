1e4890099aa3d83f3d2181c174cbbcab21186ab9
Afonso Bernardes <afonso.bernardes@tecnico.ulisboa.pt>
2023-06-05 21:11:42 +0100

    Input city, distance to center and maximum price to pay to refine searches.

--- PropertyResearch/main.py
+++ PropertyResearch/main.py
@@ -1,5 +1,4 @@
 import os
-
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -9,16 +8,16 @@ import time
 
 CHROME_DRIVER_PATH = "c:/Development/chromedriver.exe"
 OPEN_RENT_URL = "https://www.openrent.co.uk"
-FORM_LINK = "https://forms.gle/8wWgJBrvZgyBbJx96"
+FORM_LINK = "INSERT FROM LINK"
 USER_AGENT = os.environ.get("USER_AGENT")
 
 
 # SCRAPE WEBSITE FOR PROPERTIES
 def scrape_website(website_url):
 
-    # location = input("Which city do you wanna move to? ")
-    # city_area = input("How far, in kilometers, are you willing to be from city center? ")
-    # maximum_price = input("What is the maximum price you are willing to pay per month? ")
+    location = input("Which city do you wanna move to? ")
+    city_area = input("How far, in kilometers, are you willing to be from city center? ")
+    maximum_price = input("What is the maximum price you are willing to pay per month? ")
 
     headers = {"User-agent": USER_AGENT,
                "Accept-Language": "en-GB,en;q=0.9,fr;q=0.8,pt-PT;q=0.7,pt;q=0.6,es;q=0.5",
@@ -27,7 +26,6 @@ def scrape_website(website_url):
     property_parameters = {
         "term": "London",
         "area": "5",
-        "prices_min": "",
         "prices_max": "2000",
         "bedrooms_min": "0",
         "sortType": "1",  # Sort by ascending price.
@@ -38,7 +36,7 @@ def scrape_website(website_url):
                                  headers=headers)
     page_response.raise_for_status()
 
-    soup = BeautifulSoup(page_response.text, "html.parser")
+    soup = BeautifulSoup(page_response.content, "html.parser")
     property_data = soup.find(id="property-data").find_all("a")
 
     url_list = []
@@ -49,11 +47,11 @@ def scrape_website(website_url):
             address = flat.find_next('img')['alt'].split(",")
             address = " -".join(address[1:])
             url = "".join([OPEN_RENT_URL, flat['href']])
-            price = flat.find_next("h2").text.split("<span>")[0].strip("\\r\\n").split("£")[-1].split("per")[0]
+            price = flat.find_next("h2").text.split("<span>")[0].strip("\\r\\n").split("£")[-1].split("per")[0].replace(",", "")
 
             address_list.append(address)
             url_list.append(url)
-            price_list.append(price)
+            price_list.append(int(price))
 
     return address_list, url_list, price_list
 
@@ -78,6 +76,8 @@ def fill_form(form_url, flats_addresses, flats_urls, flats_prices):
 
 
 flats_address_list, flats_url_list, flats_price_list = scrape_website(website_url=OPEN_RENT_URL)
+print(flats_address_list)
+print(flats_price_list)
 fill_form(form_url=FORM_LINK, flats_addresses=flats_address_list,
           flats_urls=flats_url_list, flats_prices=flats_price_list)
 
